d5de45cfef9d0d8761568e889a4e57e2ed1cf76e
agcontr1 <alexander.ga.contreras@gmail.com>
2023-06-10 15:03:57 -0700

    more code

--- script.js
+++ script.js
@@ -16,4 +16,39 @@ function generatePassword() {
     alert(" Password will be " + numberOfCharacters + " characters long.");
   }
 
+  var lowercaseallowed = true;
+  var lowercase = "htzgyvdbkaqrcxuslpmnijofew";
   
+  var hasLowercase = confirm("Do you want lowercase characters?");
+  if (hasLowercase) {
+    alert(" Password will have lowercase characters.");
+  } else {
+    alert(" Password won't have lowercase characters.");
+  }
+  
+  var uppercaseallowed = true;
+  var uppercase = "RCKXUJVLPITODZSABNQHWFEMYG";
+  var hasUppercase = confirm("Do you want uppercase characters?");
+  if (hasUppercase) {
+    alert(" Password will have uppercase characters.");
+  } else {
+    alert(" Password won't have uppercase characters.");
+  }
+  
+  var numbers = "279850364"
+  var hasNumbers = confirm("Do you want to use numbers?");
+
+  if (hasNumbers) {
+    alert(" Password will have numbers.");
+  } else {
+    alert(" Password won't have numbers.");
+  }
+ 
+  var specialchars = "^&*!@_%()#+$#";
+  var hasSpecial = confirm("Do you want special characters?");
+  if (hasSpecial) {
+    alert("Your password will have special characters.");
+  } else {
+    alert("Your password won't have special characters.");
+  }
+
