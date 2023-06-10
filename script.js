d233b62da3a3601441661159f4825b260b0c312a
agcontr1 <alexander.ga.contreras@gmail.com>
2023-06-10 14:59:19 -0700

    if else password length

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function generatePassword() {
     var minlength = 8;
     var maxlength = 129;
     var lengthRange = minlength + '-' + maxlength;
+    if (!(numberOfCharacters >= minlength && numberOfCharacters <= maxlength))
+ {
+    alert(" Password length must be between 8 and 128 characters. Please try again.");
+    return;
+  } else {
+    alert(" Password will be " + numberOfCharacters + " characters long.");
+  }
+
+  
